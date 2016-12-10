c4b9ee8f781905652744231ae48a52c5ecf81b5b
strma17 <martin.strejc@ca.com>
2016-12-10 09:35:34 +0100

    DB Init

---
+++ db/create.sql
@@ -0,0 +1,19 @@
+-- daruj:Dar4uj
+
+CREATE ROLE daruj LOGIN ENCRYPTED PASSWORD 'md54cfa30fff93ad7039bb33be47cc25808' SUPERUSER VALID UNTIL 'infinity';
+
+CREATE DATABASE daruj
+  WITH ENCODING='UTF8'
+       OWNER=daruj
+       TEMPLATE=template0
+       LC_COLLATE='Czech_Czech Republic.1250'
+       LC_CTYPE='Czech_Czech Republic.1250'
+       CONNECTION LIMIT=-1;
+
+CREATE DATABASE daruj
+  WITH ENCODING='UTF8'
+       OWNER=daruj
+       TEMPLATE=template0
+       LC_COLLATE='cs_CZ.UTF8'
+       LC_CTYPE='cs_CZ.UTF8'
+       CONNECTION LIMIT=-1;
