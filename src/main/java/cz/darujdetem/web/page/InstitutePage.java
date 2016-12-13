ef6407ed08574d167aef9e6ec26148ff00195d3e
strma17 <martin.strejc@ca.com>
2016-12-13 14:32:44 +0100

    Institute - gifts 2 cols design

--- src/main/java/cz/darujdetem/web/page/InstitutePage.java
+++ src/main/java/cz/darujdetem/web/page/InstitutePage.java
@@ -1,13 +1,22 @@
 package cz.darujdetem.web.page;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.extensions.markup.html.repeater.data.grid.DataGridView;
+import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.PropertyListView;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.IDataProvider;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
@@ -37,6 +46,7 @@ public class InstitutePage extends AbstractDesignPage
 	@SuppressWarnings("squid:S1948")
 	private DataService dataService;
 
+	
 	public InstitutePage(PageParameters params)
 	{
 		super(params);
@@ -47,8 +57,11 @@ public class InstitutePage extends AbstractDesignPage
 		add(inst);
 		
 		inst.add(new Label("name"));
-
-		inst.add(new PersonListView("persons"));
+		
+		List<ICellPopulator<TwoPersons>> populators = new LinkedList<>();
+		populators.add(new TwoPersonPopulator((tp) -> tp.getLeft()));
+		populators.add(new TwoPersonPopulator((tp) -> tp.getRight()));
+		inst.add(new DataGridView<>("personGrid", populators, new TwoPersonDataProvider(institute.getPersons())));
 		
 	}
 
@@ -73,54 +86,137 @@ public class InstitutePage extends AbstractDesignPage
 	}
 	
 	
-	@SuppressWarnings("squid:MaximumInheritanceDepth")
-	private class PersonListView extends PropertyListView<Person> {
-
-		private static final long serialVersionUID = 1L;
-
-		public PersonListView(String id)
-		{
-			super(id);
-		}
-		
-		@Override
-		protected void populateItem(ListItem<Person> item)
-		{
-			item.add(new GiftPanel("giftPanel", item.getModel()));
-		}
-		
-	}
-	
 	private class GiftPanel extends Panel {
 
 		private static final long serialVersionUID = 1L;
 
 		public GiftPanel(String id, IModel<Person> model)
 		{
-			super(id);
+			super(id, model);
 			Person p = model.getObject();
 			Gift g = p.getGift();
+			WebMarkupContainer link;
 			if (g == null) {
-				queue(new WebMarkupContainer("link"));
+				link = new WebMarkupContainer("link");
 			} else {
-				queue(GiftPage.bookmarkablePageLink("link", g));
+				link = GiftPage.bookmarkablePageLink("link", g);
 			}
+			add(link);
+			add(new Label("name"));
+			add(new Label("gift.note"));
+			add(new Label("gift.name"));
 			
-			queue(new Label("name"));
-			queue(new Label("gift.note"));
-			queue(new Label("gift.name"));
-			queue(new Label("age"));
-			queue(new Label("ageUnit", new CzechYearUnitModel(Model.of(p.getAge()))));
-			queue(new WebMarkupContainer("male").add(new AttributeAppender("class", offerIcon(p.getMale())).setSeparator(" ")));
+			WebMarkupContainer male = new WebMarkupContainer("male");
+			male.add(new AttributeAppender("class", offerIcon(p.getMale())).setSeparator(" "));
+			add(male);
+			male.add(new Label("age"));
+			male.add(new Label("ageUnit", new CzechYearUnitModel(Model.of(p.getAge()))));
 			
 			
 			// FIXME building the path
 			String relativePath  = "/img/" + (StringUtils.isEmpty(g.getImg()) ? "" : g.getImg() );
 			
 			
-			queue(new WebMarkupContainer("img").add(new AttributeModifier("src", RequestCycle.get().getRequest().getContextPath() + relativePath)));
+			link.add(new WebMarkupContainer("img").add(new AttributeModifier("src", RequestCycle.get().getRequest().getContextPath() + relativePath)));
 			
 			
+		}
+		
+	}
+	
+	private class TwoPersons implements Serializable {
+		
+		private static final long serialVersionUID = 1L;
+		
+		private Person left;
+		private Person right;
+		
+		public Person getLeft()
+		{
+			return left;
+		}
+		
+		public void setLeft(Person left)
+		{
+			this.left = left;
+		}
+		
+		public Person getRight()
+		{
+			return right;
+		}
+		
+		public void setRight(Person right)
+		{
+			this.right = right;
+		}
+		
+	}
+	
+	private class TwoPersonPopulator implements ICellPopulator<TwoPersons> {
+
+		private static final long serialVersionUID = 1L;
+		
+		private final Function<TwoPersons, Person> personResolver;
+		
+		public TwoPersonPopulator(Function<TwoPersons, Person> personResolver)
+		{
+			this.personResolver = personResolver;
+		}
+
+		@Override
+		public void detach()
+		{
+			// do nothing
+		}
+
+		@Override
+		public void populateItem(Item<ICellPopulator<TwoPersons>> cellItem, String componentId,
+			IModel<TwoPersons> rowModel)
+		{
+			cellItem.add(new GiftPanel(componentId, new CompoundPropertyModel<>(personResolver.apply(rowModel.getObject()))));
+		}
+		
+	}
+	
+	private class TwoPersonDataProvider implements IDataProvider<TwoPersons> {
+		
+		private static final long serialVersionUID = 1L;
+		
+		private List<TwoPersons> list;
+		
+		public TwoPersonDataProvider(List<Person> persons)
+		{
+			list = new ArrayList<>(1 + persons.size() / 2);
+			TwoPersons tp = null;
+			for (Person p : persons) {
+				if (tp == null) {
+					tp = new TwoPersons();
+					list.add(tp);
+					tp.setLeft(p);
+				} else {
+					tp.setRight(p);
+					tp = null;
+				}
+			}
+		}
+
+		@Override
+		public Iterator<? extends TwoPersons> iterator(long first, long count)
+		{
+			return list.iterator();
+		}
+
+		@Override
+		public long size()
+		{
+			return list.size();
+		}
+
+		@Override
+		public IModel<TwoPersons> model(TwoPersons object)
+		{
+			return Model.of(object);
 		}
 		
 	}
