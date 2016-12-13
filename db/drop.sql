680f72f72c16cff0c36076cd236600062abc81fb
strma17 <martin.strejc@ca.com>
2016-12-13 19:03:36 +0100

    drop

---
+++ db/drop.sql
@@ -0,0 +1,7 @@
+DROP SEQUENCE public.seq_gift_id;
+DROP SEQUENCE public.seq_person_id;
+DROP SEQUENCE public.seq_institute_id;
+
+DROP TABLE public.gift;
+DROP TABLE public.person;
+DROP TABLE public.institute;
